6bad141544e29e4ca9f768e8528832893a0c2660
hibri <hibri@75285a8b-87c1-2849-90b4-784fd460a000>
2010-04-15 16:31:39 +0000

    added
    git-svn-id: svn://svn.7digital.local/SevenDigital.API/oauth_helper@39436 75285a8b-87c1-2849-90b4-784fd460a000

---
+++ ApiPostRequestBuilder.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using SevenDigital.Security.OAuth.Signature;
+
+namespace OAuthSig
+{
+	public class ApiPostRequestBuilder
+	{
+		public string Build(bool oAuthSignRequest, Uri fullyQualifiedUrl, string postParams, string oAuthConsumerKey, string oAuthConsumerSecret, string signature, string timeStamp, string nonce, string oAuthVersion, string tokenKey, string tokenSecret)
+		{
+			try
+			{
+			WebClient client = new WebClient();
+			if (oAuthSignRequest)
+			{
+				client.Headers.Add("Authorization", GetSignedAuthorizationHeader(fullyQualifiedUrl, postParams, oAuthConsumerKey,  signature,  timeStamp,  nonce, oAuthVersion, oAuthConsumerSecret, tokenKey, tokenSecret));
+			}
+			else
+			{
+				IDictionary<string, string> values = new Dictionary<string, string>();
+				values.Add(OAuthQueryParameters.CONSUMER_KEY_KEY, oAuthConsumerKey);
+				client.Headers.Add("Authorization", BuildOAuthHeaderString(values));
+			}
+		
+				client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+				return client.UploadString(fullyQualifiedUrl, postParams);
+			}catch(WebException ex) {
+				return ex.Message;
+			}
+		}
+
+		private void AddTo(string key, string parameter, IDictionary<string, string> dictionary)
+		{
+			if (false == String.IsNullOrEmpty(parameter))
+			{
+				dictionary.Add(key, HttpUtility.UrlEncode(parameter));
+			}
+		}
+
+		private string BuildOAuthHeaderString(IDictionary<string, string> oAUthParameters)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("OAuth ");
+			foreach (var kvp in oAUthParameters)
+			{
+				sb.AppendFormat("{0}=\"{1}\",", kvp.Key, kvp.Value);
+			}
+			sb.Remove(sb.Length - 1, 1);
+			return sb.ToString();
+		}
+
+		private Dictionary<string, string> GetFormVariables(string postParams)
+		{
+			if (string.IsNullOrEmpty(postParams))
+			{
+				return new Dictionary<string, string>();
+			}
+			IEnumerable<string> enumerable = postParams.Split('&').Select(s => s);
+			return enumerable.ToDictionary(str => str.Split('=')[0], str => str.Split('=')[1]);
+		}
+
+		private string GetHeader(string oAuthVersion, string nonce, string timeStamp, string signature, string oAuthConsumerKey)
+		{
+			IDictionary<string, string> oAuthParameters = new Dictionary<string, string>();
+			AddTo(OAuthQueryParameters.VERSION_KEY, oAuthVersion, oAuthParameters);
+			AddTo(OAuthQueryParameters.NONCE_KEY, nonce, oAuthParameters);
+			AddTo(OAuthQueryParameters.TIMESTAMP_KEY, timeStamp, oAuthParameters);
+			AddTo(OAuthQueryParameters.SIGNATURE_METHOD_KEY, OAuthQueryParameters.HMACSHA1_SIGNATURE_TYPE, oAuthParameters);
+			AddTo(OAuthQueryParameters.CONSUMER_KEY_KEY, oAuthConsumerKey, oAuthParameters);
+			AddTo(OAuthQueryParameters.SIGNATURE_KEY, signature, oAuthParameters);
+
+			return BuildOAuthHeaderString(oAuthParameters);
+		}
+
+		public string Normalize(IDictionary<string, string> parameters)
+		{
+			SortedDictionary<string, string> sortedParams = this.Sort(parameters);
+			StringBuilder result = new StringBuilder();
+			int i = 0;
+			foreach (string key in sortedParams.Keys)
+			{
+				i++;
+				result.AppendFormat("{0}={1}", key, sortedParams[key]);
+				if (i < sortedParams.Count)
+				{
+					result.Append("&");
+				}
+			}
+			return result.ToString();
+		}
+
+		private SortedDictionary<string, string> Sort(IEnumerable<KeyValuePair<string, string>> queryParameters)
+		{
+			SortedDictionary<string, string> parameters = new SortedDictionary<string, string>();
+			foreach (KeyValuePair<string, string> par in queryParameters)
+			{
+				parameters.Add(par.Key, par.Value);
+			}
+			return parameters;
+		}
+
+		public string GetSignedAuthorizationHeader(Uri url, string postParams, string oAuthConsumerKey, string signature, string timeStamp, string nonce, string oAuthVersion, string consumerSecret, string tokenKey, string tokenSecret)
+		{
+			Dictionary<string, string> dictionary = GetFormVariables(postParams);
+
+			var sig = new SevenDigital.Security.OAuth.Signature.SignatureGenerator();
+			string sigs = sig.Generate(url, dictionary, oAuthConsumerKey, consumerSecret, tokenKey, tokenSecret, "POST", timeStamp, nonce, SignatureGenerator.SignatureTypes.HmacSha1, "1.0");
+
+			string header = GetHeader(oAuthVersion, nonce, timeStamp, sigs, oAuthConsumerKey);
+
+			return header;
+		}
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OAuthSig
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            OAuth.OAuthBase.SignatureTypes signatureType = OAuth.OAuthBase.SignatureTypes.HMACSHA1;
+            string normalizedUrl;
+            string normalizedRequestParameters;
+            OAuth.OAuthBase myOAuth = new OAuth.OAuthBase();
+
+            try
+            {
+                Uri uri = new Uri(txtURI.Text + postVariablesTextBox.Text);
+			
+                string consumerKey = txtConsKey.Text;
+                string consumerSecret = txtConsSecret.Text;
+                string token = txtToken.Text;
+                string tokenSecret = txtTokenSecret.Text;
+                string httpMethod = drpHTTPMethod.SelectedItem.ToString();
+                string timeStamp = txtTimestamp.Text;
+                string nonce = txtNonce.Text;
+
+                if (string.IsNullOrEmpty(timeStamp))
+                {
+                    timeStamp = myOAuth.GenerateTimeStamp();
+                    txtTimestamp.Text = timeStamp;
+                }
+
+                if (string.IsNullOrEmpty(nonce))
+                {
+                    nonce = myOAuth.GenerateNonce();
+                    txtNonce.Text = nonce;
+                }
+
+                switch (drpSigMethod.SelectedIndex)
+                {
+                    case 0:
+                        signatureType = OAuth.OAuthBase.SignatureTypes.HMACSHA1;
+                        break;
+                    case 1:
+                        signatureType = OAuth.OAuthBase.SignatureTypes.PLAINTEXT;
+                        break;
+                    case 2:
+                        signatureType = OAuth.OAuthBase.SignatureTypes.RSASHA1;
+                        break;
+                }
+
+                myOAuth.includeVersion = chkVersion.Checked;
+
+                string signature = myOAuth.GenerateSignature(uri, consumerKey, consumerSecret,
+                    token, tokenSecret, httpMethod, timeStamp, nonce, signatureType,
+                    out normalizedUrl, out normalizedRequestParameters);
+
+                txtRawSig.Text = signature;
+                txtEncodedSig.Text = myOAuth.UrlEncode(signature);
+
+                txtGenURL.Text = normalizedUrl + "?" + normalizedRequestParameters + "&oauth_signature=" + txtEncodedSig.Text;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            drpSigMethod.SelectedIndex = 0;
+            drpHTTPMethod.SelectedIndex = 0;
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+		private void makeRequestButton_Click(object sender, EventArgs e)
+		{
+			    string httpMethod = drpHTTPMethod.SelectedItem.ToString();
+			if(string.IsNullOrEmpty(txtGenURL.Text) ) return;
+
+			if (httpMethod == "POST") {
+				ApiPostRequestBuilder apiPostRequestBuilder = new ApiPostRequestBuilder();
+				Uri uri = new Uri(txtURI.Text);
+				responseText.Text = apiPostRequestBuilder.Build(true, uri , postVariablesTextBox.Text, txtConsKey.Text, txtConsSecret.Text, txtEncodedSig.Text, txtTimestamp.Text, txtNonce.Text, "1.0", txtToken.Text, txtTokenSecret.Text);
+			} else {
+				WebClient webClient = new WebClient();
+				string response = webClient.DownloadString(txtGenURL.Text);
+				responseText.Text = response;
+			}
+
+		}
+    }
+}
