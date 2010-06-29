6b15bb5eaf26654334022a91c7c70c3249d16543
Hibri Marzook <hibri@hibri.net>
2010-06-29 09:42:12 +0100

    spaces to tabs

--- OAuthRequest.cs
+++ OAuthRequest.cs
@@ -3,69 +3,60 @@ using System.Net;
 
 namespace OAuthSig
 {
-    internal class OAuthRequest
-    {
-        private readonly IView _view;
+	internal class OAuthRequest
+	{
+		private readonly IView _view;
 
-        public OAuthRequest(IView view)
-        {
-            _view = view;
-        }
+		public OAuthRequest(IView view)
+		{
+			_view = view;
+		}
 
-        public void Request()
-        {
-            string httpMethod = _view.HttpMethod;
-            if (String.IsNullOrEmpty(_view.GeneratedUrl))
-            {
-                return;
-            }
+		public void Request()
+		{
+			string httpMethod = _view.HttpMethod;
+			if (String.IsNullOrEmpty(_view.GeneratedUrl))
+			{
+				return;
+			}
 
-            if (httpMethod == "POST")
-            {
-                DoHttpPost();
-            }
-            else
-            {
-                DoHttpGet();
-            }
+			if (httpMethod == "POST")
+			{
+				DoHttpPost();
+			}
+			else
+			{
+				DoHttpGet();
+			}
 
-            _view.Log();
+			_view.Log();
+		}
 
-        }
+		private void DoHttpGet()
+		{
+			var webClient = new WebClient();
+			string response = webClient.DownloadString(_view.GeneratedUrl);
+			_view.DisplayResponse(response);
+		}
 
-        private void DoHttpGet()
-        {
-            try
-            {
-                WebClient webClient = new WebClient();
-                string response = webClient.DownloadString(_view.GeneratedUrl);
-                _view.DisplayResponse(response);
-            }
-            catch (WebException wex)
-            {
-
-                TestHelper.FireLogMessage(wex.Message);
-            }
-        }
-
-        private void DoHttpPost()
-        {
-            try
-            {
-
-                ApiPostRequestBuilder apiPostRequestBuilder = new ApiPostRequestBuilder();
-                Uri uri = new Uri(_view.Uri);
-                string response = apiPostRequestBuilder.Build(true,
-                                                              uri, _view.PostData,
-                                                              _view.ConsumerKey,
-                                                              _view.ConsumerSecret, _view.Token,
-                                                              _view.TokenSecret, _view.RawSignature, _view.Nonce,
-                                                              _view.TimeStamp);
-                _view.DisplayResponse(response);
-            }catch(WebException wex)
-            {
-                TestHelper.FireLogMessage(wex.Message);
-            }
-        }
-    }
+		private void DoHttpPost()
+		{
+			try
+			{
+				var apiPostRequestBuilder = new ApiPostRequestBuilder();
+				var uri = new Uri(_view.Uri);
+				string response = apiPostRequestBuilder.Build(true,
+				                                              uri, _view.PostData,
+				                                              _view.ConsumerKey,
+				                                              _view.ConsumerSecret, _view.Token,
+				                                              _view.TokenSecret, _view.RawSignature, _view.Nonce,
+				                                              _view.TimeStamp);
+				_view.DisplayResponse(response);
+			}
+			catch (WebException wex)
+			{
+				TestHelper.FireLogMessage(wex.Message);
+			}
+		}
+	}
 }
--- OAuthSignatureBuilder.cs
+++ OAuthSignatureBuilder.cs
@@ -5,84 +5,87 @@ using OAuth;
 
 namespace OAuthSig
 {
-    internal  class OAuthSignatureBuilder
-    {
-        private readonly IView _view;
+	internal class OAuthSignatureBuilder
+	{
+		private readonly IView _view;
 
-        public OAuthSignatureBuilder(IView view)
-        {
-            this._view = view;
-        }
+		public OAuthSignatureBuilder(IView view)
+		{
+			_view = view;
+		}
 
-        public  void GenerateSignature()
-        {
+		public void GenerateSignature()
+		{
+			OAuthBase.SignatureTypes signatureType = OAuthBase.SignatureTypes.HMACSHA1;
+			string normalizedUrl = null;
+			string normalizedRequestParameters = null;
+			var myOAuth = new OAuthBase();
+			try
+			{
+				var uri = new Uri(_view.Uri);
 
+				string consumerKey = _view.ConsumerKey;
+				string consumerSecret = _view.ConsumerSecret;
+				string token = _view.Token;
+				string tokenSecret = _view.TokenSecret;
+				string httpMethod = _view.HttpMethod;
+				string timeStamp = _view.TimeStamp;
+				string nonce = _view.Nonce;
 
-            OAuthBase.SignatureTypes signatureType = OAuthBase.SignatureTypes.HMACSHA1;
-            string normalizedUrl = null;
-            string normalizedRequestParameters = null;
-            OAuthBase myOAuth = new OAuthBase();
-            try {
-                Uri uri = new Uri(_view.Uri);
+				if (String.IsNullOrEmpty(timeStamp))
+				{
+					timeStamp = myOAuth.GenerateTimeStamp();
+					_view.TimeStamp = timeStamp;
+				}
 
-                string consumerKey = _view.ConsumerKey;
-                string consumerSecret = _view.ConsumerSecret;
-                string token = _view.Token;
-                string tokenSecret = _view.TokenSecret;
-                string httpMethod = _view.HttpMethod;
-                string timeStamp = _view.TimeStamp;
-                string nonce = _view.Nonce;
+				if (String.IsNullOrEmpty(nonce))
+				{
+					nonce = myOAuth.GenerateNonce();
+					_view.Nonce = nonce;
+				}
 
-                if (String.IsNullOrEmpty(timeStamp)) {
-                    timeStamp = myOAuth.GenerateTimeStamp();
-                    _view.TimeStamp = timeStamp;
-                }
+				switch (_view.SignatureMethod)
+				{
+					case 0:
+						signatureType = OAuthBase.SignatureTypes.HMACSHA1;
+						break;
+					case 1:
+						signatureType = OAuthBase.SignatureTypes.PLAINTEXT;
+						break;
+					case 2:
+						signatureType = OAuthBase.SignatureTypes.RSASHA1;
+						break;
+				}
 
-                if (String.IsNullOrEmpty(nonce)) {
-                    nonce = myOAuth.GenerateNonce();
-                    _view.Nonce = nonce;
-                }
+				myOAuth.includeVersion = _view.IncludeVersion;
 
-                switch (_view.SignatureMethod) {
-                    case 0:
-                        signatureType = OAuthBase.SignatureTypes.HMACSHA1;
-                        break;
-                    case 1:
-                        signatureType = OAuthBase.SignatureTypes.PLAINTEXT;
-                        break;
-                    case 2:
-                        signatureType = OAuthBase.SignatureTypes.RSASHA1;
-                        break;
-                }
+				string signature = "";
+				if (httpMethod == "POST")
+				{
+					Dictionary<string, string> dictionary = new ApiPostRequestBuilder().GetFormVariables(_view.PostData);
+					signature = myOAuth.GenerateSignature(uri, consumerKey, consumerSecret, token, tokenSecret, httpMethod,
+					                                      timeStamp, nonce, signatureType, out normalizedUrl,
+					                                      out normalizedRequestParameters, dictionary);
+				}
+				else
+				{
+					signature = myOAuth.GenerateSignature(uri, consumerKey, consumerSecret,
+					                                      token, tokenSecret, httpMethod,
+					                                      timeStamp, nonce, signatureType,
+					                                      out normalizedUrl,
+					                                      out normalizedRequestParameters, null);
+				}
 
-                myOAuth.includeVersion = _view.IncludeVersion;
-                
-                string signature = "";
-                if (httpMethod == "POST")
-                {
-                    Dictionary<string, string> dictionary = new ApiPostRequestBuilder().GetFormVariables(_view.PostData);
-                    signature = myOAuth.GenerateSignature(uri, consumerKey, consumerSecret, token, tokenSecret, httpMethod,
-                                                          timeStamp, nonce, signatureType, out normalizedUrl, out normalizedRequestParameters, dictionary);
-                     
-                }else
-                {
-                    signature = myOAuth.GenerateSignature(uri, consumerKey, consumerSecret,
-                                                          token, tokenSecret, httpMethod,
-                                                          timeStamp, nonce, signatureType,
-                                                          out normalizedUrl,
-                                                          out normalizedRequestParameters, null);
-                }
+				_view.RawSignature = signature;
+				_view.EncodedSignature = myOAuth.UrlEncode(signature);
 
-                _view.RawSignature = signature;
-                _view.EncodedSignature = myOAuth.UrlEncode(signature);
-
-                _view.GeneratedUrl = normalizedUrl + "?" + normalizedRequestParameters +
-                                    "&oauth_signature=" + _view.EncodedSignature;
-            } catch (Exception exception) {
-                MessageBox.Show(exception.Message);
-            }
-        }
-
-  
-    }
+				_view.GeneratedUrl = normalizedUrl + "?" + normalizedRequestParameters +
+				                     "&oauth_signature=" + _view.EncodedSignature;
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.Message);
+			}
+		}
+	}
 }
