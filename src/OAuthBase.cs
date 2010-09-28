6efc5008c2fdfbe8bc0f3a08bb7f52f5b59b7703
Hibri Marzook <hibri@hibri.net>
2010-09-28 11:29:44 +0100

    should url encode request parameters

--- src/OAuthBase.cs
+++ src/OAuthBase.cs
@@ -162,7 +162,7 @@ namespace OAuth {
             QueryParameter p = null;
             for (int i = 0; i < parameters.Count; i++) {
                 p = parameters[i];
-                sb.AppendFormat("{0}={1}", p.Name, p.Value);
+                sb.AppendFormat("{0}={1}", p.Name, UrlEncode( p.Value));
 
                 if (i < parameters.Count - 1) {
                     sb.Append("&");
